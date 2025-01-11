d1ebbb521f72bedd53766e1f2b2e27349dc4464e
neerajkhatri04 <neerajkha304@gmail.com>
2025-01-11 22:38:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("trained_plant_disease_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+#Sidebar
+st.sidebar.title("Plant Disease Detection System for Sustainable Agriculture")
+app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
+
+from PIL import Image
+img = Image.open("th.jpg")
+
+st.image(img)
+
+#Main Page
+if(app_mode=="HOME"):
+    st.markdown("<h1 style='text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html=True)
+    
+#Prediction Page
+elif(app_mode=="DISEASE RECOGNITION"):
+    st.header("Plant Disease Detection System for Sustainable Agriculture")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy']
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
